8714ef98a6633c6a03c5ea21e8096fbe0d671409
silviuvulcan <silviu.vulcan@gmail.com>
2014-08-29 10:34:10 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-bashmytv
-========
 
-Script to show episodes to aquire and watch
+Simple script to print episodes to watch & aquire using myepisodes.com as a backend.
+
+You need an account at http://www.myepissodes.com, after you login go to http://www.myepisodes.com/rsshelp.php#unwatched to find your feed links. It's up to you if you use showignored=0 or showignored=1, it works either way
+
+* You need curl installed
---
+++ bashmytv.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# unaquired feed
+UNAQRSS='http://www.myepisodes.com/rss.php?feed=unacquired&showignored=0&uid=xxx&pwdmd5=xxx'
+# unwatched feed
+UNWARSS='http://www.myepisodes.com/rss.php?feed=unwatched&showignored=0&uid=xxx&pwdmd5=xxx'
+
+UNQ=$(curl -s $UNAQRSS | grep '<title>\[' | sed -e 's/<[^>]\+>//g' -e 's/^[ \t]*//' -e 's/\]\[/-/g' -e 's/\[\ //g' -e 's/\ \]//g')
+UNW=$(curl -s $UNWARSS | grep '<title>\[' | sed -e 's/<[^>]\+>//g' -e 's/^[ \t]*//' -e 's/\]\[/-/g' -e 's/\[\ //g' -e 's/\ \]//g')
+
+if [ -z $UNQ ] || [ -z $UNW ]; then
+    echo "Something went wrong, maybe the feed address is wrong? Try running both this commands:"
+    echo
+    echo -e "\t curl '$UNAQRSS'"
+    echo -e "\t curl '$UNWARSS'"
+    echo
+    echo "both should generate output."
+    exit 1
+fi
+
+UNQNO=$(echo "$UNQ"|wc -l)
+UNWNO=$(echo "$UNW"|wc -l)
+
+if [ "$UNWNO" = "200" ]; then UNWNO="200+"; fi		# myepisodes.com limts this feed to 200 items
+
+echo "$UNQNO episode$([ "$UNQNO" = "1" ] && echo " needs" || echo "s need") to be aquired:"
+echo "$UNQ"
+echo
+
+echo "$UNWNO episode$([ "$UNWNO" != "1" ] && echo "s") to watch"
+echo "$UNW"
